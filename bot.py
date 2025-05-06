e431a73cdd2605b640070aa651c8f9ac803a5813
SUKEsann2000 <SUKEsann2000@users.noreply.github.com>
2025-05-06 16:51:10 +0900

    diagコマンドを追加し、ラズパイの情報を表示する埋め込みメッセージを実装

--- bot.py
+++ bot.py
@@ -16,4 +16,19 @@ async def on_ready():
     print("起動完了")
     await tree.sync()#スラッシュコマンドを同期
 
+@tree.command()
+async def diag(interaction: discord.Interaction):
+    embed = discord.Embed(title="状況",
+                      description="ラズパイの今の情報\n\nここにoutputを書く",
+                      colour=0x00b0f4)
+
+    embed.set_author(name="Raspi Info",
+                 icon_url="https://www.wireguard.com/img/wireguard.svg")
+
+    embed.add_field(name="A New Field",
+                value="",
+                inline=False)
+
+    await interaction.response.send_message(embed=embed)
+
 client.run(TOKEN)
